21be5e3ff4e741788ed5b1caf87841a25f712a51
kalzc <mail2chakri@gmail.com>
2020-11-16 01:12:50 +0800

    debug add

---
+++ 0x-debug/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "1.103.1"
+    }
+  }
+}
+
+provider "alicloud" {
+  # Configuration options
+  region = "ap-southeast-1"
+}
+
+
+data "alicloud_enhanced_nat_available_zones" "enhanced"{
+}
+
+output "nat" {
+    value = data.alicloud_enhanced_nat_available_zones.enhanced
+}
